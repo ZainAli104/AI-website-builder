032f6ed5e881e3929c7074ed2d2caa24221ff6b9
ZainAli <chzainali6363@gmail.com>
2025-07-07 22:07:58 +0500

    changes

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import {Button} from "@/components/ui/button";
+
+const Home = () => {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1 className="text-4xl font-bold">Welcome to My Next.js App!</h1>
+            <p className="mt-4 text-lg">This is a simple example of a Next.js application.</p>
+            <Button>Test</Button>
+        </main>
+    );
+};
+
+export default Home;
