e562e65f2cf9f8d62623cb1b5b952f8911ad33f3
ZainAli <chzainali6363@gmail.com>
2025-10-28 15:10:34 +0500

    feat✨: integrate @e2b/code-interpreter for sandbox management

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,3 +1,4 @@
+import {Sandbox} from "@e2b/code-interpreter";
 import {Agent, openai, createAgent} from "@inngest/agent-kit";
 
 import {inngest} from "./client";
@@ -5,7 +6,12 @@ import {inngest} from "./client";
 export const helloWorld = inngest.createFunction(
     {id: "hello-world"},
     {event: "test/hello.world"},
-    async ({event}) => {
+    async ({event, step}) => {
+        const sanboxId = await step.run("get-sandbox-id", async () => {
+            const sandbox = await Sandbox.create("vibe-nextjs-zain-3");
+            return sandbox.sandboxId;
+        });
+
         const codeAgent = createAgent({
             name: "code-agent",
             system: "You are an expert Next.js developer. You write readable, maintainable, and efficient code." +
@@ -15,7 +21,11 @@ export const helloWorld = inngest.createFunction(
 
         const {output} = await codeAgent.run(
             `Write the following snippet: ${event.data.value}`,
-        )
+        );
+
+        // const sandboxUrl = await step.run("get-sandbox-url", async () => {
+        //     const sandbox =
+        // });
 
         return {output};
     },
